c9097660740002be939ff7025a307b8e09e0c531
Rishit1378 <rishit.chawla2022@vitstudent.ac.in>
2025-11-13 04:52:50 +0530

    Fix: Update to new public Cognito App Client ID

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -2,8 +2,8 @@
 const awsmobile = {
   aws_project_region: "ap-south-1", // e.g., "us-east-1"
   aws_cognito_region: "ap-south-1",
-  aws_user_pools_id: "ap-south-1_Tzy7BhW08",
-  aws_user_pools_web_client_id: "4blumjvro57ukdjp7678bdj135",
+  aws_user_pools_id: "ap-south-1_2GWT5ukzJ",
+  aws_user_pools_web_client_id: "1idevg6hu804eje4pl59s3b042",
   API: {
     REST: {
       CloudStoreAPI: {
